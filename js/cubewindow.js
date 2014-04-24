42c1d6dd0d83cc0944fe0a6ae8bb70833580e061
kidcompassion <sallypoulsen@gmail.com>
2014-04-24 04:13:01 -0600

    adds psd and flickr query

--- js/cubewindow.js
+++ js/cubewindow.js
@@ -3,9 +3,15 @@ $(document).ready(function(){
 	$('.view').click(function(){
 		
 		var number = Math.floor((Math.random() *5));
-		var imgArray = ['yellow', 'blue', 'green', 'red', 'purple'];
+		var imgArray = [
+			'yellow', 
+			'blue', 
+			'green', 
+			'red', 
+			'purple'
+			];
 
-		$('.window').css('background', imgArray[number]);
+		$('.window').css('background-image', imgArray[number]);
 	});
 	
 });
