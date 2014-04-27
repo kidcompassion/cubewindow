0e6c3b518cb62de4d7b39e3105cb2c5aee07250d
kidcompassion <sallypoulsen@gmail.com>
2014-04-26 20:00:23 -0600

    Turns the array of photos into an array of objects.

--- js/cubewindow.js
+++ js/cubewindow.js
@@ -1,25 +1,43 @@
 $(document).ready(function(){
 	
 	$('.view').click(function(){
+				$(".credit_link").remove();
 		var imgArray = [
-			"carillonjane", 
-			"mary", 
-			"olgapolis", 
-			"contessabessa",
-			"tomfrommyspace",
-			"dora",
-			"videogirl",
-			"cijin",
-			"argleton", 
-			"241"
+			{
+				"photo_name" : "carillonjane", 
+				"photo_credit" : "Photo by someone", 
+				"photo_url" : "http://twitter.com" 
+			},  
+			{
+				"photo_name" : "mary", 
+				"photo_credit" : "Photo by @pipingloud", 
+				"photo_url" : "http://twitter.com/pipingloud" 
+			},
+			{
+				"photo_name" : "olgapolis", 
+				"photo_credit" : "Photo by @olgapolis", 
+				"photo_url" : "http://twitter.com/olgapolis" 
+			}, 
+			{
+				"photo_name" : "contessabessa", 
+				"photo_credit" : "Photo by contessabessa", 
+				"photo_url" : "http://twitter.com/contessabessa"
+			}
 			];
 
 		var totalImgs = imgArray.length;
 
 		var number = Math.floor((Math.random() * totalImgs));
+		var credit = '<a class="credit_link" href="' + imgArray[number].photo_url + '" target="_blank">' + imgArray[number].photo_credit + '</a>';
 
 
-		$(".window").css("background-image", "url('img/" + imgArray[number] + ".jpg')");
+		$(".window").css("background-image", "url('img/" + imgArray[number].photo_name + ".jpg')");
+
+		$(".container").prepend(credit);
+
+
+
+
 	});
 	
 });
